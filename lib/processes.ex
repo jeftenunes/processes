74be312559ba2cd888177922897caf1f0120c55e
jeftenunes <jeftenunes@hotmail.com>
2024-07-14 17:08:06 -0300

    create a generic crud server

--- lib/processes.ex
+++ lib/processes.ex
@@ -24,4 +24,17 @@ defmodule Processes do
 
     async_qry.(qry)
   end
+
+  def bulk_long_running_queries, do: Enum.each(1..5, fn i ->
+    long_running_qry(i)
+  end)
+
+  def listen_loop() do
+    receive do
+      :first -> IO.inspect("first")
+      :second -> IO.inspect("second")
+    after
+      2000 -> send(self(), :first)
+    end
+  end
 end
